148006cd5b164cf7b05ee2ddbf77518f39ecb434
Adrian Wan <awan1@cs.swarthmore.edu>
2014-11-09 22:22:03 -0500

    Checking in skeleton code. Now need to install volley to make HTTP request.

--- RequestTest/app/src/main/java/edu/swarthmore/cs/lab3/requesttest/RequestTest.java
+++ RequestTest/app/src/main/java/edu/swarthmore/cs/lab3/requesttest/RequestTest.java
@@ -4,16 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class RequestTest extends Activity {
+
+    private Button mRequestButton;
+    private TextView mRequestResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_test);
-    }
 
+        // Find buttons and TextViews
+        mRequestButton = (Button) findViewById(R.id.request_button);
+        mRequestResponse = (TextView) findViewById(R.id.request_response);
+
+        // Button listeners
+        mRequestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRequestResponse.setText("Here's your response!");
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
