370ad8d2ff6d66bddac75ee06e6fcb2d93ec77bb
Adrian Wan <awan1@cs.swarthmore.edu>
2014-11-10 00:00:59 -0500

    App is able to query shim server.

---
+++ RequestTest/app/src/main/java/edu/swarthmore/cs/lab3/requesttest/RequestClient.java
@@ -0,0 +1,74 @@
+package edu.swarthmore.cs.lab3.requesttest;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.apache.http.HttpStatus;
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by awan1 on 11/9/14.
+ */
+
+public class RequestClient extends AsyncTask<String, String, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+        URL url;
+        HttpURLConnection urlConnection = null;
+        String response = "";
+
+        try {
+            url = new URL(params[0]);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            int responseCode = urlConnection.getResponseCode();
+
+            if(responseCode == HttpStatus.SC_OK){
+                response = readStream(urlConnection.getInputStream());
+                Log.v("RequestClient", response);
+            }else{
+                Log.v("RequestClient", "Response code:"+ responseCode);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(urlConnection != null)
+                urlConnection.disconnect();
+        }
+
+        return response;
+    }
+
+    private String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuffer response = new StringBuffer();
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return response.toString();
+    }
+}
+
+
--- RequestTest/app/src/main/java/edu/swarthmore/cs/lab3/requesttest/RequestTest.java
+++ RequestTest/app/src/main/java/edu/swarthmore/cs/lab3/requesttest/RequestTest.java
@@ -2,11 +2,38 @@ package edu.swarthmore.cs.lab3.requesttest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.RequestConnControl;
+import org.json.JSONArray;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 public class RequestTest extends Activity {
@@ -27,9 +54,22 @@ public class RequestTest extends Activity {
         mRequestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mRequestResponse.setText("Here's your response!");
+                makeRequest();
             }
         });
+    }
+
+    private void makeRequest() {
+        RequestClient client = new RequestClient();
+        String response = null;
+        try {
+            response = client.execute("http://130.58.68.129:8083/data/fitbit/steps?username=superdock&dateStart=2014-10-20&dateEnd=2014-10-22").get();
+        } catch (InterruptedException e) {
+            response = "Interrupted Exception caught.";
+        } catch (ExecutionException e) {
+            response = "Execution Exception caught.";
+        }
+        mRequestResponse.setText(response);
     }
 
     @Override
